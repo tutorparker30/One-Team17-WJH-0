d66280b8b42f52956363e0a39b7c59f0d3779557
Wonjihui <jt3624@naver.com>
2025-02-15 19:35:29 +0900

    WJH: MyActor 클래스 리펙토림

--- Source/test/Private/MyActor.cpp
+++ Source/test/Private/MyActor.cpp
@@ -1,18 +1,15 @@
 
 #include "MyActor.h"
 
-// Sets default values
 AMyActor::AMyActor()
 {
- 	
 	PrimaryActorTick.bCanEverTick = false;
-
 }
+
 void AMyActor::BeginPlay()
 {
 	Super::BeginPlay();
 
-	UE_LOG(LogTemp, Log, TEXT("Written by QWE."));
+	UE_LOG(LogTemp, Log, TEXT("Written by PGH!!!"));
 }
-
 
