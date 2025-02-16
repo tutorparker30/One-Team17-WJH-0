f0c9a1198ae63557c14975a48f314abafacc61db
Wonjihui <jt3624@naver.com>
2025-02-16 23:54:50 +0900

    remove

--- Source/SpartaProject/SpartaProject.cpp
+++
@@ -1,6 +0,0 @@
-// Copyright Epic Games, Inc. All Rights Reserved.
-
-#include "SpartaProject.h"
-#include "Modules/ModuleManager.h"
-
-IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, SpartaProject, "SpartaProject" );
